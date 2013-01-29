9e68cdc1b75975dc372ad4e4817662d258530b02
Sebastian Hasait <sebastian@hasait.de>
2013-01-29 23:25:08 +0100

    pom

--- src/main/java/de/hasait/clap/CLAPHelpCategory.java
+++ src/main/java/de/hasait/clap/CLAPHelpCategory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 by HasaIT (hasait at web dot de)
+ * Copyright (C) 2013 by Sebastian Hasait (sebastian at hasait dot de)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
